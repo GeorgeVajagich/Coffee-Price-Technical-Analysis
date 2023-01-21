6ba7ffc588e877b58e7e89d3cf62b0c2cb49d24b
GeorgeVajagich <56782795+GeorgeVajagich@users.noreply.github.com>
2023-01-20 23:55:00 -0600

    Add files via upload

--- Coffee Price Script.R
+++ Coffee Price Script.R
@@ -1,10 +1,3 @@
----
-  title: "Coffee Price Technical Analysis"
-author: "George Vajagich"
-date: "11/30/2022"
-output: html_document
----
-
 knitr::opts_chunk$set(echo = TRUE)
 library(readr)
 options(readr.show_col_types = FALSE)
@@ -12,8 +5,7 @@ library(ggplot2)
 library(lubridate)
 library(scales)
 library(dplyr)
-
-
+library(grid)
 
 
 
@@ -23,26 +15,37 @@ df$date<- mdy(df$date)
 
 
 
+
 ggplot(data = df, aes(x = date, y = value, group = 1))+
   geom_line()+
   scale_x_date(breaks="5 years", labels = date_format("%Y"))
-
 
 
 head(df)
 
 
 
-df <- df %>% # now shift it
-  mutate(one_day_past = lag(value,n=1))
+PVF <- function (days){
+  
+  df <- df %>% mutate(past = lag(value,n=days))
+  df <- df %>% mutate_at(c("value"), tibble::lst("future"=lead), n = days)
+  
+  df <- df %>% mutate(next_performance=(future/value-1)*100)
+  df  <- df %>% mutate(past_performance=(value/past-1)*100)
+  
+  grob = grobTree(textGrob(paste("Correlation : ", round(cor(df$past_performance, df$next_performance, use = "complete.obs"),4)), x = 0.75, y = 0.95, gp = gpar(col = "red", fontsize = 12)))
+  
+  print(cor(df$past_performance, df$next_performance, use = "complete.obs")) 
+  
+  ggplot(df, aes(x=past_performance, y=next_performance)) + geom_point(size=0.5) + annotation_custom(grob) + theme(aspect.ratio=1)
+  
+}
 
+PVF(1)
+PVF(5)
+PVF(20)
+PVF(240)
+PVF(480)
+PVF(1200)
 
-
-
-df %>% mutate_at(c("value"), tibble::lst("one_day_future"=lead), n = 1)
-
-
-
-
-df  %>% mutate(P1Dperformance=(value/one_day_past-1)*100)
 
